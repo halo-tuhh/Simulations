4999d53a7557eae2c363202de6aa6774ff922664
FinnBreu <finn.breuer@tuhh.de>
2026-01-04 20:51:12 +0100

    corrections

--- Eos/main.py
+++ Eos/main.py
@@ -7,7 +7,7 @@ Created on Sun Dec  7 19:20:42 2025
 
 "Parameters"
 # Operations
-ops_temp_celsius = 20 # deg C, initial tank temperature after filling
+ops_temp_celsius = 0 # deg C, initial tank temperature after filling
 ops_fuel_name = 'Ethanol' # for fluid property lookup
 ops_pressurant_name = 'N2' # for fluid property lookup
 ops_pressurizing_enable = 0 # boolean, enable inert gas pressurization on the pad
@@ -83,7 +83,7 @@ chamber_expansion = chamber_exit_area / chamber_throat_area # nozzle expansion r
 gravity = 9.81 # m/s^2
 air_R = 287.07 # surrounding air gas constant 
 air_pressure_zero = 101325.0  # Pa, standard pressure at MSL in Pascal
-air_temp = 273.15 + ops_temp_celsius # K
+ops_temp = 273.15 + ops_temp_celsius # K
 ops_pressurant_mass = 0.0 # kg, mass of inert press gas. Set to zero in case pressurization is disabled
 
 
@@ -92,7 +92,7 @@ ops_pressurant_mass = 0.0 # kg, mass of inert press gas. Set to zero in case pre
 def injector_ox_hem(T1, P1, P2): # K tank temperature, Pa tank pressure, Pa chamber pressure
     ### Two-phase Homogeneous Equilibrium Model
     dm = 0 # kg/s, mass flow rate
-    ### Ox 
+    ### Tank condition
     if P1 > propsi ("P", "T", T1, "Q", 0, "N2O") + 1e3 :
         h1 = propsi ("H", "T", T1, "P", P1, "N2O")
         s1 = propsi ("S", "T", T1, "P", P1, "N2O")
@@ -122,7 +122,7 @@ def injector_ox_hem(T1, P1, P2): # K tank temperature, Pa tank pressure, Pa cham
 def injector_ox_hem_gas(T1, P1, P2): # K tank temperature, Pa tank pressure, Pa chamber pressure
     ### One-phase ideal gas model
     dm = 0 # kg/s, mass flow rate   
-    ### Ox condition
+    ### Tank condition
     rho = propsi ("D", "T|gas", T1, "P", P1, "N2O")  
     cp = propsi ("Cpmass", "T|gas", T1, "P", P1, "N2O")
     cv = propsi ("Cvmass", "T|gas", T1, "P", P1, "N2O")
@@ -158,20 +158,18 @@ def chamber(dm_ox, dm_fuel): # kg/s, mass flow rates
     dm_ox = abs(dm_ox)
     dm_fuel = abs(dm_fuel)
     dm = dm_ox + dm_fuel
-    if dm_fuel > 0.01:
+    if dm_fuel > 0.001: # only if fuel is still flowing
         dm_ratio = dm_ox / dm_fuel
         Pc = 1e5 # Pa, chamber pressure. Initial guess with arbitrary atmospheric
-        for i in range(0, 10, 1): # run couple of times to converge Pc
+        for i in range(0, 10, 1): # run couple of times to converge chamber pressure
             c_star_opt = C.get_Cstar(Pc=Pc, MR=dm_ratio)       
             c_star = c_star_opt * chamber_cstar_efficiency
             Pc = c_star * dm / chamber_throat_area
         expansion = air_pressure_zero / Pc * C.get_PcOvPe(Pc=Pc, MR=dm_ratio, eps=chamber_expansion, frozen=0, frozenAtThroat=0)
-        isp_amb = C.estimate_Ambient_Isp(Pc=Pc, MR=dm_ratio, eps=chamber_expansion, Pamb=air_pressure_zero)[0]
+        isp_amb = chamber_nozzle_efficiency * C.estimate_Ambient_Isp(Pc=Pc, MR=dm_ratio, eps=chamber_expansion, Pamb=air_pressure_zero)[0]
     else:
-        Pc = 0.0; c_star = 0.0; isp_amb = 0.0
-        
+        Pc = 0.0; c_star = 0.0; isp_amb = 0.0; expansion=0.0
     F = dm * isp_amb # N, thrust force
-        
     return F, Pc, c_star, isp_amb, expansion # N thrust force, Pa chamber pressure, m/s combustion efficiency, m/s specific impulse, 
 
 
@@ -204,7 +202,7 @@ def tank_init_heating(y0, P_target): # initial state, Pa firing pressure target
 def tank_init_pressurizing(y0, P_target): # initial state, Pa firing pressure target
     # inert gas pressurization to firing pressure  after closing vent valve
     # assumes press on pad only -> constant press mass
-    m, mf, T = y0 # ox mass, fuel mass, tank temperature  
+    m, mf, T = y0 # ox mass, fuel mass, tank temperature     
     
     rho = m/tank_ox_vol # ox density
     P = propsi ("P", "T", T, "D", rho, "N2O")  # oxidizer tank pressure   
@@ -218,14 +216,13 @@ def tank_init_pressurizing(y0, P_target): # initial state, Pa firing pressure ta
     if x < 0: x = 0 
     if x > 1: x = 1
     m_vap = m * x # kg, ox vapor mass
-
+    
     if P_press > 1e3: # only do if significant difference
         V_press = m_vap / rho_vap # only in ox gaseous phase
         rho_press = propsi ("D", "T", T, "P", P_press, ops_pressurant_name) # kg/m^3, inert gas initial density    
         m_press = rho_press * V_press
     else:
-        m_press = 0
-        
+        m_press = 0       
     return m_press
 
 
@@ -242,7 +239,7 @@ def engine_tank_eq(y, ydot, P_ch, step):
     
     ### End condition
     if m < 0.01 or T < 100: # empty tank or too cold for coolprop
-        return y, ydot, 0.0, 0.0, 0.0, 0.0
+        return y, ydot, 0.0, 0.0, 0.0, 0.0, 0.0
     
     ### Ox tank 
     rho = m/tank_ox_vol # ox density
@@ -301,13 +298,9 @@ def engine_tank_eq(y, ydot, P_ch, step):
 
 "Run simulation"
 ### Setup
-T_0 = 283
+T_0 = ops_temp
 t_end = 15
 step = 0.02
-
-ops_pressurizing_enable = 0 # boolean, enable inert gas pressurization on the pad
-ops_heating_enable = 0 # boolean, enable tank heating on the pad
-ops_pressure_target = 5e6 # Pa, firing pressure. Requires either presurization or heating enabled
 
 y0, P, x = tank_init_eq(T_0, tank_ox_vol, tank_ox_ullage)
 if ops_heating_enable:
